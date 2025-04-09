a425ef6f1a4531bb19e316b57027bb09192e8a77
THENDO NETSHANDAMA <tnetshandama90@gmail.com>
2025-04-09 04:09:06 +0200

    Update Openfiles.py

--- Openfiles.py
+++ Openfiles.py
@@ -1,6 +1,16 @@
-# Open the file in read mode
-with open('example.txt', 'r') as file:
-    content = file.read()
-
-# Print the content of the file
-print(content)
+#reading the file and printing it's contents
+try:
+    #reading existing file
+    filename = input("BM.txt:")
+    with open(BM.txt, "r") as file:
+        content = file.read()
+        print(content) 
+#creating and writing a new file
+    GOMD = input("BM.txt:")
+    new_content = input("GOMD.txt:")
+    with open(GOMD, 'w') as file:
+        mod_file = content + '\n' + new_content
+        file.write(mod_file)
+    print(f"created successfully {mod_file}")
+except FileNotFoundError: 
+    print("file not found/error")
